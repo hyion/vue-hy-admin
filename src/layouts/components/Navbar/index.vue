a70218b0eb7fb945ffbf190735a012a4c1782f1a
donxbei <gxhz0416@163.com>
2021-01-06 18:04:05 +0800

    navbar

--- src/layouts/components/Navbar/index.vue
+++ src/layouts/components/Navbar/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div></div>
+  <div class="navbar">
+    <Breadcrumb class="breadcrumb-container" />
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import { defineComponent, onMounted, reactive, toRefs } from 'vue';
+import Breadcrumb from './Breadcrumb.vue';
+
+export default defineComponent({
+  components: { Breadcrumb },
+  setup() {
+    const state = reactive({});
+
+    onMounted(() => {});
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
 </script>
 
-<style>
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .breadcrumb-container {
+    float: left;
+  }
+}
 </style>
