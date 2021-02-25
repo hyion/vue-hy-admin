581fba46c12724e66f8f6ec2d68a264a7add49b1
panherui <724776286@qq.com>
2021-02-25 22:37:15 +0800

    首页优化

--- src/views/Article/detail.vue
+++ src/views/Article/detail.vue
@@ -61,144 +61,147 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, unref, toRefs, onMounted } from 'vue';
-import { GetArticleDetail, CreateArticle } from '/@/api';
-import { useRoute, useRouter } from 'vue-router';
-import { useMessage } from '/@/hooks/useMessage';
-import { parseTime } from '/@/hooks';
+  import { defineComponent, reactive, ref, unref, toRefs, onMounted } from 'vue';
+  import { GetArticleDetail, CreateArticle } from '/@/api';
+  import { useRoute, useRouter } from 'vue-router';
+  import { useMessage } from '/@/hooks/useMessage';
+  import { parseTime } from '/@/hooks';
 
-export default defineComponent({
-  name: 'ArticleDetail',
-  setup() {
-    const formRef = ref<any>(null);
-    const state = reactive({
-      formData: {
-        title: '',
-        content: '',
-        describe: '',
-        url: '',
-        time: '',
-      },
-      tmpTime: '',
-      height: window.innerHeight - 200,
-      isLoading: false,
-    });
-    const route = useRoute();
-    const router = useRouter();
+  export default defineComponent({
+    name: 'ArticleDetail',
+    setup() {
+      const formRef = ref<any>(null);
+      const state = reactive({
+        formData: {
+          title: '',
+          content: '',
+          describe: '',
+          url: '',
+          time: '',
+          image: {
+            url: '',
+          },
+        },
+        tmpTime: '',
+        height: window.innerHeight - 200,
+        isLoading: false,
+      });
+      const route = useRoute();
+      const router = useRouter();
 
-    const rules = reactive({
-      title: [{ required: true, message: '请填写标题', trigger: 'blur' }],
-      content: [{ required: true, message: '请填写内容', trigger: 'blur' }],
-      describe: [{ required: true, message: '请填写描述', trigger: 'blur' }],
-      url: [{ required: true, message: '请填写上传背景封面', trigger: 'blur' }],
-      time: [{ required: true, message: '请选择发布时间', trigger: 'change' }],
-    });
+      const rules = reactive({
+        title: [{ required: true, message: '请填写标题', trigger: 'blur' }],
+        content: [{ required: true, message: '请填写内容', trigger: 'blur' }],
+        describe: [{ required: true, message: '请填写描述', trigger: 'blur' }],
+        url: [{ required: true, message: '请填写上传背景封面', trigger: 'blur' }],
+        time: [{ required: true, message: '请选择发布时间', trigger: 'change' }],
+      });
 
-    const { notification } = useMessage();
+      const { notification } = useMessage();
 
-    onMounted(() => {
-      if (route.params.id !== 'add') {
-        getData();
-      }
-    });
+      onMounted(() => {
+        if (route.params.id !== 'add') {
+          getData();
+        }
+      });
 
-    const getData = () => {
-      state.isLoading = true;
-      GetArticleDetail(route.params.id)
-        .then((res: any) => {
-          const data = res.body;
-          console.log('GetArticleDetail--', data);
-          state.formData = data;
-          state.isLoading = false;
-        })
-        .catch((e) => {
-          console.log(e);
-          state.isLoading = false;
-        });
-    };
-
-    const imgUpload = () => {
-      console.log('imgUpload');
-    };
-
-    const onSubmit = async () => {
-      const form = unref(formRef);
-      if (!form) return;
-      await form.validate((valid: boolean) => {
-        if (!valid) return;
+      const getData = () => {
         state.isLoading = true;
-        console.log(state.formData);
-        CreateArticle(state.formData)
-          .then((res) => {
-            console.log(res);
+        GetArticleDetail(route.params.id)
+          .then((res: any) => {
+            const data = res.body;
+            console.log('GetArticleDetail--', data);
+            state.formData = data;
             state.isLoading = false;
-            notification('success', '创建成功');
-            router.push('/article');
           })
-          .catch(() => {
+          .catch((e) => {
+            console.log(e);
             state.isLoading = false;
           });
-      });
-    };
+      };
 
-    const selectTime = (time: any) => {
-      state.formData.time = parseTime(time, '{y}/{m}/{d} {h}:{i}');
-    };
+      const imgUpload = () => {
+        console.log('imgUpload');
+      };
 
-    return {
-      ...toRefs(state),
-      formRef,
-      rules,
-      imgUpload,
-      onSubmit,
-      selectTime,
-    };
-  },
-});
+      const onSubmit = async () => {
+        const form = unref(formRef);
+        if (!form) return;
+        await form.validate((valid: boolean) => {
+          if (!valid) return;
+          state.isLoading = true;
+          console.log(state.formData);
+          CreateArticle(state.formData)
+            .then((res) => {
+              console.log(res);
+              state.isLoading = false;
+              notification('success', '创建成功');
+              router.push('/article');
+            })
+            .catch(() => {
+              state.isLoading = false;
+            });
+        });
+      };
+
+      const selectTime = (time: any) => {
+        state.formData.time = parseTime(time, '{y}/{m}/{d} {h}:{i}');
+      };
+
+      return {
+        ...toRefs(state),
+        formRef,
+        rules,
+        imgUpload,
+        onSubmit,
+        selectTime,
+      };
+    },
+  });
 </script>
 
 <style lang="scss" scoped>
-.article-detail {
-  padding: 25px;
-  .header {
-    margin-bottom: 10px;
-  }
-  .upload-box {
-    display: flex;
-    margin: 10px -7px 6px;
-    .upload-demo {
-      width: 50%;
-      max-width: 360px;
-      margin: 0 7px;
-      &::deep .el-upload {
-        width: 100%;
-        .el-upload-dragger {
+  .article-detail {
+    padding: 25px;
+    .header {
+      margin-bottom: 10px;
+    }
+    .upload-box {
+      display: flex;
+      margin: 10px -7px 6px;
+      .upload-demo {
+        width: 50%;
+        max-width: 360px;
+        margin: 0 7px;
+        &::deep .el-upload {
           width: 100%;
-          .el-icon-picture-outline-round,
-          .el-icon-headset {
-            font-size: 46px;
-            color: #c0c4cc;
-            margin: 42px 0 14px;
-            line-height: 50px;
-            transition: all 0.3s;
-          }
-          .el-upload__text {
-            color: #98999c;
-            transition: all 0.3s;
-          }
-          &:hover {
+          .el-upload-dragger {
+            width: 100%;
             .el-icon-picture-outline-round,
-            .el-upload__text,
             .el-icon-headset {
-              color: #409eff;
+              font-size: 46px;
+              color: #c0c4cc;
+              margin: 42px 0 14px;
+              line-height: 50px;
+              transition: all 0.3s;
+            }
+            .el-upload__text {
+              color: #98999c;
+              transition: all 0.3s;
+            }
+            &:hover {
+              .el-icon-picture-outline-round,
+              .el-upload__text,
+              .el-icon-headset {
+                color: #409eff;
+              }
             }
           }
         }
       }
-    }
-    img {
-      width: 100%;
+      img {
+        width: 100%;
+      }
     }
   }
-}
 </style>
--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <p style="word-break: break-all">token: {{ token }}</p>
+  <div class="home custom-space">
+    <h2>
+      <span class="el-icon-magic-stick"></span>
+      万物皆有裂痕，那是光照进来的地方
+    </h2>
+    <div class="container">
+      <section class="info">
+        <h4>Hello，你好，我是谁！</h4>
+        <div>
+          <p>重新认识、审视、定义一下自己吧。</p>
+          <ul>
+            <li><span class="el-icon-ice-drink"></span>现在的你，是个怎样的人？</li>
+            <li><span class="el-icon-lollipop"></span>你希望以后成为怎样的人？</li>
+            <li><span class="el-icon-lollipop"></span>你心中的未来是怎样的？</li>
+            <li><span class="el-icon-lollipop"></span>最想做的一件事是什么？</li>
+            <li><span class="el-icon-lollipop"></span>你现在的生活，开心吗？</li>
+            <li><span class="el-icon-hot-water"></span>心如止水，淡中得味，加油。</li>
+          </ul>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -24,5 +43,105 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style lang="scss" scoped>
+.home {
+  position: relative;
+  margin: 20px;
+  padding: 10px;
+  h2 {
+      width: 100%;
+      font-size: 16px;
+      font-weight: 400;
+      margin: 10px 0 30px;
+      letter-spacing: 1px;
+      color: #0e8dff;
+      span {
+        color: #0e8bff;
+        font-size: 20px;
+      }
+    }
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    section {
+      width: calc(50% - 15px);
+      display: inline-block;
+      height: 300px;
+      box-shadow: 0 2px 20px #e0f0ff;
+      border-radius: 20px;
+      background: #fff;
+      padding: 20px;
+      margin-bottom: 24px;
+      position: relative;
+      h3 {
+        font-size: 18px;
+        color: #7fc3ff;
+        font-weight: 400;
+        text-transform: uppercase;
+        position: relative;
+        height: 20px;
+        margin-bottom: 20px;
+        &::before {
+          content: '';
+          height: 1px;
+          width: 26px;
+          background: #c7dcf3;
+          position: absolute;
+          bottom: -2px;
+        }
+      }
+      &.info {
+        h4 {
+          font-size: 16px;
+          font-weight: 400;
+          margin: 22px 0 10px;
+          color: #0e8bff;
+          padding-left: 15px;
+          &::after {
+            content: '\e6a4';
+            font-family: element-icons !important;
+            speak: none;
+            font-style: normal;
+            font-weight: 400;
+            font-variant: normal;
+            text-transform: none;
+            line-height: 1;
+            vertical-align: baseline;
+            display: inline-block;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            position: absolute;
+            left: 14px;
+            top: 10px;
+            font-size: 30px;
+            color: #7fc3ff;
+          }
+        }
+        p {
+          font-size: 13px;
+          color: #909399;
+          padding-left: 15px;
+        }
+        ul {
+          margin: 10px 0;
+          padding: 14px 15px 12px;
+          border-radius: 10px;
+          border: 1px solid #edf3f9;
+          li {
+            position: relative;
+            color: #5182b3;
+            line-height: 24px;
+            font-size: 13px;
+            letter-spacing: 1px;
+            span {
+              margin-right: 4px;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
